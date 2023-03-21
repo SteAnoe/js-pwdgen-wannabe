dac2604917ca2f06dacb34b80f7881c382f84ab6
SteAnoe <ste.anoe@gmail.com>
2023-03-21 13:48:16 +0100

    finito e più bello

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,4 @@ const nome = prompt("Qual'è il tuo nome?");
 const cognome = prompt("Qual'è il tuo cognome?");
 const colore = prompt("Qual'è il tuo colore preferito?");
 document.getElementById('password').value = `${nome}${cognome}${colore}21`;
+console.log('bella');
